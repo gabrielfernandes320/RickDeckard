c70a1a2fd42b22c083c240bfff7a71f564877fbe
gabrielfernandes320 <gabrielfernandesfg@hotmail.com>
2019-05-23 00:09:58 -0300

    Creation of ConectionsReplicationDAO

---
+++ src/database/ConectionsReplicationDAO.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ConectionsReplicationDAO extends MasterDAO {
+    @Override
+    public List<Object> SelectAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Object Select(Object parameter) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void Update(Object parameter) throws SQLException {
+
+    }
+
+    @Override
+    public void Insert(Object parameter) throws SQLException {
+
+    }
+
+    @Override
+    public int Delete(Object parameter) throws SQLException {
+        return 0;
+    }
+}
